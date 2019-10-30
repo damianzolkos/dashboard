df7711c3215f39faf7a0ce45b6cbe0b1500b21fa
Damian Żółkoś <damianzolkos@gmail.com>
2019-10-30 16:11:39 +0100

    some changes xd
    media query in css, sidebar menu button, example module update, js changes

--- js/script.js
+++ js/script.js
@@ -14,11 +14,13 @@ function onLoad(){
     console.log("%c Modules:", "font-weight: bold;");
     console.table(boxes);
     console.log("");console.log("");console.log("");
-
+    
     // initial config of the app
     window.document.title = appName;
     document.getElementById("logo").innerHTML = appName;
     // ###########################
+
+    czas();
 
         for (let i = 0; i<pages.length; i++) {
             createSidebarMenuItem(pages[i]);
@@ -31,6 +33,18 @@ function onLoad(){
         }
         notificationsCounterUpdate();
     }
+
+function czas() {
+        getTime();
+        if ($('#datetime').length > 0) {
+            document.getElementById("hour").innerHTML = h;
+            document.getElementById("minutes").innerHTML = m;
+            document.getElementById("day").innerHTML = da;
+            document.getElementById("month").innerHTML = mo;
+            document.getElementById("year").innerHTML = y;
+        }
+        setTimeout(czas, 1000);
+}
 function changeScreen(nameOfScreen) {
         for (let i = 0; i<pages.length; i++) {
             document.getElementById(pages[i]+"_menuItem").className = "inactive";
@@ -92,7 +106,7 @@ function newNotification(message) {
     messages[notifications] = message;
     notifications++;
     var newMessageLi = document.createElement("li");
-    newMessageLi.innerHTML = message;
+    newMessageLi.innerHTML = h + ":" + m + " - " + message;
     document.getElementById("notifications_list").appendChild(newMessageLi);
     notificationsCounterUpdate();
 }
@@ -104,4 +118,24 @@ function clearNotifications() {
     $("#notifications_list").children(":not(#noNotifications)").remove();
     notificationsCounterUpdate();
     notificationsWindowToggle();
+}
+
+const mq = window.matchMedia( "(max-width: 500px)" );
+if (mq.matches == true) {
+    showOrHide = 0;
+} else showOrHide = 1;
+
+function showSidebar() {
+    if (showOrHide == 0) {
+        document.getElementById('sidebar').style.transform = 'translate(0px,0px)';
+        document.getElementById('content').style.left = "250px";
+        document.getElementById('content').style.width = 'calc(100% - 250px)';
+        showOrHide = 1;
+    }
+    else if (showOrHide == 1) {
+        document.getElementById('sidebar').style.transform = 'translate(-250px,0px)';
+        document.getElementById('content').style.left = "0px";
+        document.getElementById('content').style.width = '100%';
+        showOrHide = 0;
+    }
 }
--- modules/config.js
+++ modules/config.js
@@ -1,11 +1,13 @@
 var appName = "Template";
 
 // pages = ['Page 1', 'Page 2'];
-var pages = ['Page 1'];
+var pages = ['Page 1', 'Page 2'];
 
 
 //  	page name,  unique(!) id,   module name
-//	eq. ['Page 1', 'example-module', 'example_module'],
+//	eq. ['Page 1', 'example', 'example'],
 var boxes = [
-    ['Page 1', 'example-module', 'example_module'],
+    ['Page 1', 'example', 'example'],
+    ['Page 2', 'example-2', 'example'],
+    ['Page 2', 'example-3', 'example'],
 ];
