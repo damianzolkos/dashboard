7108f66fde6b458b0fe3c67028ea862ccbb37244
Damian Żółkoś <damianzolkos@gmail.com>
2019-12-29 13:23:13 +0100

    added 'about' module

--- modules/config.js
+++ modules/config.js
@@ -1,12 +1,16 @@
 var appName = "Dashboard";
+var appVersion = "0.1";
+var author = "Damian Żółkoś";
+var authorLink = "http://github.com/damianzolkos";
 
 // pages = ['Page 1', 'Page 2'];
-var pages = ['Page 1', 'Settings'];
-
+var pages = ["Page 1", "Settings", "ReadMe"];
 
 //   page name,  unique(!) id,   module name
 //	eq. ['Page 1', 'example', 'example'],
 var boxes = [
-    ['Page 1', 'example', 'example'],
-    ['Settings', 'modules', 'modules'],
+  ["Page 1", "example", "example"],
+  ["Settings", "modules", "modules"],
+  ["ReadMe", "readme", "readme"],
+  ["Settings", "about", "about"],
 ];
